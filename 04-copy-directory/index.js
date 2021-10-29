3c25c67c268968f7ddc9b50ac4b626d7785096f9
LenarFF <flfforwork@gmail.com>
2021-10-29 16:38:05 +0300

    feat: add copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const fsPromises = require("fs/promises");
+const path = require('path');
 
+fsPromises.mkdir(path.join(__dirname, "files-copy"), { recursive: true });
+
+function callback(err) {
+  if (err) throw err;
+  console.log("source.txt was copied to destination.txt");
+}
+
+const getFiles = async () => {
+  try {
+    const items = await fsPromises.readdir(path.join(__dirname, "files"));
+    items.forEach((item) =>
+      fs.copyFile(path.join(__dirname, "files", item), path.join(__dirname, "files-copy", item), callback)
+    );
+    console.log(items);
+  } catch (err) {
+    console.error(err);
+  }
+};
+getFiles();
